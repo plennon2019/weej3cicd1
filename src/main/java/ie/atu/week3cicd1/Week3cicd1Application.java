905655a618d0ef01de3661d2c67055d27079cff7
Paul Lennon <paul.lennon@gmit.ie>
2024-10-01 10:18:09 +0100

    New project

---
+++ src/main/java/ie/atu/week3cicd1/Week3cicd1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3cicd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3cicd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week3cicd1/Week3cicd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3cicd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
